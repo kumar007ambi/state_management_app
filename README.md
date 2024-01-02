f3de030609a41ff8211e2cac6ae932ac17fe051b
kumar007ambi <60788663+kumar007ambi@users.noreply.github.com>
2024-01-02 05:25:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# state_management_app
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/kumar007ambi/state_management_app)
---
+++ src/test.ts
@@ -0,0 +1,20 @@
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
